4e0491481f0b2759fb540b47addc05d8288bf49f
Thisma08 <77389133+Thisma08@users.noreply.github.com>
2023-01-25 14:34:51 +0100

    Image s'affiche dans 2 fenêtres (Une pour OpenCv, une pour Pygame)

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import pygame as pg
 import cv2
+
+class Convertisseur:
+    def __init__(self, chemin='img/HandsomeSquidward.jpg'):
+        pg.init()
+        self.chemin = chemin
+        self.image = cv2.imread(self.chemin)
+        self.RESOLUTION = self.larg, self.haut = self.image.shape[0], self.image.shape[1]
+        self.surface = pg.display.set_mode(self.RESOLUTION)
+        self.horloge = pg.time.Clock()
+
+    def prendre_image(self):
+        pass
+
+    def dessiner(self):
+        pg.surfarray.blit_array(self.surface, self.image)
+        cv2.imshow('img', self.image)
+
+    def execute(self):
+        while True:
+            for e in pg.event.get():
+                if e.type == pg.QUIT:
+                    exit()
+            self.dessiner()
+            pg.display.set_caption(str(self.horloge.get_fps()))
+            self.horloge.tick()
+
+if __name__ == '__main__':
+    app = Convertisseur()
+    app.execute()

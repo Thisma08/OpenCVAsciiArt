8bf1aaa0e08447c948f05fd07ef0d4db10808d57
Thisma08 <77389133+Thisma08@users.noreply.github.com>
2023-01-25 14:44:30 +0100

    Image s'affiche dans 2 fenêtres (Une pour OpenCv, une pour Pygame) (Probleme réglé dans la fenêtre "OpenCV")

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ class Convertisseur:
     def __init__(self, chemin='img/HandsomeSquidward.jpg'):
         pg.init()
         self.chemin = chemin
-        self.image = cv2.imread(self.chemin)
+        self.image = self.prendre_image()
         self.RESOLUTION = self.larg, self.haut = self.image.shape[0], self.image.shape[1]
         self.surface = pg.display.set_mode(self.RESOLUTION)
         self.horloge = pg.time.Clock()
 
     def prendre_image(self):
-        pass
+        self.cv2_image = cv2.imread(self.chemin)
+        image_transposee = cv2.transpose(self.cv2_image)
+        image_rgb = cv2.cvtColor(image_transposee, cv2.COLOR_RGB2BGR)
+        return image_rgb
+
+    def dessiner_cv2_image(self):
+        cv2_image_redim = cv2.resize(self.cv2_image, (640, 360), interpolation=cv2.INTER_AREA)
+        cv2.imshow('img', cv2_image_redim)
 
     def dessiner(self):
         pg.surfarray.blit_array(self.surface, self.image)
-        cv2.imshow('img', self.image)
+        self.dessiner_cv2_image()
 
     def execute(self):
         while True:
@@ -24,6 +31,7 @@ class Convertisseur:
                     exit()
             self.dessiner()
             pg.display.set_caption(str(self.horloge.get_fps()))
+            pg.display.flip()
             self.horloge.tick()
 
 if __name__ == '__main__':
